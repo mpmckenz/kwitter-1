f2f4843aa4ed3d77ea8309b7be3090b49ee46168
Doug Enas <douglassenas@gmail.com>
2018-11-29 14:20:34 -0500

    added navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import Navbar from './navbar.jsx'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <p>Kwitter</p>
+
+      <div>
+        <Switch>
+        <Route path='*' component={Navbar}/>
+      </Switch>
       </div>
+      
+      
     );
   }
 }
 
-export default App;
+export default withRouter((App));
