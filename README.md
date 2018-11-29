5e27c7c0aec98f5d6982820dec112e2d416e995a
Doug Enas <douglassenas@gmail.com>
2018-11-29 13:30:58 -0500

    app works locally

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 Doug- Scrum Master
-Jen- QA TEster
+Jen- QA Tester
 Michael- Product Owner
 
-<!-- TODO: react app inititiated DONE -->
+General Notes:
+1. I'm going to start creating an outline on the project tab in the repo.
+2. It'll look pretty general at first but as soon as we start digging into it oI'll be able to specify more.
+3. Feel free to slack me to add/ edit any backlog items. I think he wanted to delineate tasks so I'll continue to be the one to manage them
+<!-- TODO: create-react-app inititiated DONE -->
+
+TODO: npm install --save react-redux
+TODO: npm install --save connected-react-router
+TODO: npm install --save redux
+
 
 TODO: create skeleton of kwitter
-Different React pages to render
-
-1. Sign in page
-2. sign up page
-3. Once user signs up either render back to login page or auto login
+Different React pages to render:
+1. Sign-in page
+2. sign-up page
+3. home page (available in header nav bar)
+4. edit profile page (available in header nav bar)
+5. Search
 
 
 Features:
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -10,3 +10,53 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+/*
+  This code sets up Redux devtools and connected-react-router
+  . 
+  I have provided it so you do not have to go through the all the setup yourself.
+  However, it would be instructive to go through the setup on your own if you have time. 
+  If you would like to go through setup on your own you can see the instructions at the two links below:
+    https://github.com/zalmoxisus/redux-devtools-extension
+    https://github.com/supasate/connected-react-router
+  
+  
+  Quick Setup Instructions
+  DONE 1.) replace your src/index.js with the below code
+  DONE 2.) npm install --save connected-react-router
+  3.) make sure you have a file ./reducers/index.js because it is being imported like that on line 22
+  4.) depending on how your project is strutured, there may be other imports that you have to fix the file path for
+*/
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import { Provider } from "react-redux";
+// import todoApp from "./reducers";
+// import registerServiceWorker from "./registerServiceWorker";
+// import "todomvc-app-css/index.css";
+// import { createBrowserHistory } from "history";
+// import { connectRouter, routerMiddleware } from "connected-react-router";
+// import { ConnectedRouter } from "connected-react-router";
+
+// const history = createBrowserHistory();
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(
+//   connectRouter(history)(todoApp),
+//   composeEnhancers(
+//     applyMiddleware(
+//       routerMiddleware(history) // for dispatching history actions
+//     )
+//   )
+// );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <ConnectedRouter history={history}>
+//       <App />
+//     </ConnectedRouter>
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+// registerServiceWorker();
