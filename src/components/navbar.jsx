ff470f46726ef3ce5a4d260f19cb0b4e54db6141
Michael McKenzie <mikemckenzie987@gmail.com>
2018-11-30 02:20:18 -0500

    JS and CSS for navBar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../styling/navBar.css";
 
 class Navbar extends React.Component {
-    render () {
-        return (
-            <p>Navbar</p>
-        )
-    }
+  render() {
+    return (
+      <div className="navWrapper">
+        <ul className="navBar">
+          {/* Use ternary to assign className="isVisible" */}
+          <li>
+            <a href="/">Login</a>
+          </li>
+          <li>
+            <a href="/posts">News Feeds</a>
+          </li>
+          <li>
+            <a href="/edit">Edit Profile</a>
+          </li>
+          <li>
+            <a href="/home">Homepage</a>
+          </li>
+          <li>
+            <a href="/register">Signup</a>
+          </li>
+          <li>
+            <a href="/">Logout</a>
+          </li>
+          <br />
+          {/* <ul> */}
+          <li>
+            <img
+              src="https://im4.ezgif.com/tmp/ezgif-4-8578d02997f9.gif"
+              alt=""
+            />
+          </li>
+          <li className="navTitle">Kwitter</li>
+        </ul>
+        {/* </ul> */}
+      </div>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
 /*
