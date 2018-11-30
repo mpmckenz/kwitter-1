3b6b3b120f9bc64b6bb57cf6fc4a12a45c942011
Doug Enas <douglassenas@gmail.com>
2018-11-30 15:49:38 -0500

    added css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,44 +1,26 @@
 import React, { Component } from "react";
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
+import { Button, Form, Header } from 'semantic-ui-react';
+import "../styling/login.css"
 
 export default class Login extends Component {
   render() {
     return (
-      <Grid textAlign='center' style={{ height: '100%', verticalAlign:'flex-start', marginLeft:'100px' }}>
-        <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='teal' textAlign='center'>
-              Log-in to your account
+      <div className="login">
+      <Header as='h2' color='blue' textAlign='center'>
+              Log-in to Kwitter
             </Header>
-            <Form size='large'>
-              <Segment 
-                stacked         
-                >
-                <Form.Input
-                  fluid
-                  required
-                  autoFocus
-                  
-                  className="username"
-                  placeholder='Username'
-                  onChange= {this.handleChangeUser}
-                  
-                  />
-                <Form.Input
-                  fluid
-                  required
-                  className="password"
-                  placeholder='Password'
-                  type='password'
-                  onChange= {this.handleChangePassword}
-                />
-                <Button fluid size='large' onClick={this.handleSubmitLogin}>
-                  Login
-                </Button>
-              </Segment>
-            </Form>
-            
-        </Grid.Column>
-        </Grid>
+      <Form size="large">
+    <Form.Field>
+      
+      <input placeholder='Username' />
+    </Form.Field>
+    <Form.Field>
+      
+      <input placeholder='Password' />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
+        </div>
       
     );
   }
