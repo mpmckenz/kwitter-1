c74b577e0ef83565f83395193c0a6b13b77b7041
Doug Enas <douglassenas@gmail.com>
2018-11-30 15:10:26 -0500

    added semantic-ui functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,44 @@
 import React, { Component } from "react";
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
 
 export default class Login extends Component {
   render() {
     return (
-      
-        <div>
-        <form>
-  <label>
-    Username:
-    <input type="text" name="name" />
-  </label>
-  <label>
-    Password:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" onSubmit={this.handleLoginSubmit} />
-  {/* handleLoginSubmit renders login page */}
-</form>   
-        </div>
+      <Grid textAlign='center' style={{ height: '100%', verticalAlign:'flex-start', marginLeft:'100px' }}>
+        <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='teal' textAlign='center'>
+              Log-in to your account
+            </Header>
+            <Form size='large'>
+              <Segment 
+                stacked         
+                >
+                <Form.Input
+                  fluid
+                  required
+                  autoFocus
+                  
+                  className="username"
+                  placeholder='Username'
+                  onChange= {this.handleChangeUser}
+                  
+                  />
+                <Form.Input
+                  fluid
+                  required
+                  className="password"
+                  placeholder='Password'
+                  type='password'
+                  onChange= {this.handleChangePassword}
+                />
+                <Button fluid size='large' onClick={this.handleSubmitLogin}>
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+            
+        </Grid.Column>
+        </Grid>
       
     );
   }
