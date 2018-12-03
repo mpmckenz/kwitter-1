a1361a8265373b0f82c0c28cb3b4cffe2d0e8c08
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-12-03 16:59:36 -0500

    added styling and worked on form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,27 +1,72 @@
 import React, { Component } from 'react';
+import { Button, Form, Header } from 'semantic-ui-react';
+import { connectRouter } from 'connected-react-router';
+import '../styling/register.css';
 
-export class Register extends Component {
+export default class Register extends Component {
     state = {
-        displayName: "",
         username: "",
         password: "",
         passwordRepeat: "",
         passwordMatches: true
     }
 
-    
+    handleRegister = () => {
+        if (
+            this.state.username &&
+            this.state.password === this.state.passwordRepeat
+        ) {
+            this.setState({ passwordMatches: true })
+            this.props.registerUser(this.state.username, this.state.password)
+
+            return
+        }
+
+        if (this.state.password !== this.state) {
+            this.setState({ passwordMatches: false })
+        } else {
+            this.setState({ passwordMatches: true })
+        }
+    }
+
+    noMatch = () => {
+        return (
+            <Segment color="red">Entered passwords do not match</Segment>
+        )
+    }
 
     render() {
         return (
-            <div>
-                <div>
-                    <label for=""></label>
-                </div>
-                <div>
-                    <input onSubmit={this.handleRegister} />
-                </div>
+            <div className="register">
+                <Header className="header" as="h2">Sign Up</Header>
+                <Form size="large">
+                    <Form.Field>
+                        <input className="input" placeholder="Username" value={this.state.username} fluid required autoFocus />
+                    </Form.Field>
+                    <Form.Field>
+                        { this.state.passwordMatches ? null : this.noMatch() }
+                        <input className="input" placeholder="Password" value={this.state.password} fluid required />
+                    </Form.Field>
+                    <Form.Field>
+                        <input className="input" placeholder="Re-Enter Password" value={this.state.passwordRepeat} fluid required />
+                    </Form.Field>
+                    <Button className="submit-button" type="submit" onSubmit={this.handleRegister}>Register</Button>
+                </Form>
             </div>
         );
     }
 }
-  
+
+const mapStateToProps = ({ register }) => ({
+    register
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        registerUser: (username, password) => {
+            dispatch(registerUser(username, password))
+        }
+    }
+}
+
+export default connectRouter(mapStateToProps, mapDispatchToProps)(Register)
