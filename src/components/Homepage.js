1bbf6a52718e131f763df36cabf31fb36e29a4bb
Michael McKenzie <mikemckenzie987@gmail.com>
2018-12-05 15:11:12 -0500

    added default user photo, linked, ui library, homepage css

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,27 +1,49 @@
 import React, { Component } from "react";
-import { Card, Icon, Image, Feed } from "semantic-ui-react";
+import { Card, Icon, Image, Feed, Input } from "semantic-ui-react";
 import octojpg from "../octo1.png";
 
 class Homepage extends Component {
   render() {
     return (
-      <Card>
-        <Image src={octojpg} />
-        <Card.Content>
-          <Card.Header>Display Name</Card.Header>
-          <Card.Meta>
-            <span className="date">User Name</span>
-          </Card.Meta>
-          <Card.Description>
-            All about me, blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah
-          </Card.Description>
-        </Card.Content>
-        <br />
-        <Card.Content extra>
-          <Icon name="user">0 posts</Icon>
-        </Card.Content>
-      </Card>
+      <div className="userHomepage">
+        <div className="userCard">
+          <Card>
+            <Image src={octojpg} />
+            <div className="userHomepageInfo">
+              <Card.Content>
+                <Card.Header>Display Name</Card.Header>
+                <Card.Meta>
+                  <span className="date">User Name</span>
+                </Card.Meta>
+                <Card.Description>
+                  All about me, blah blah blah blah blah blah blah blah blah
+                  blah blah blah blah blah blah blah
+                </Card.Description>
+              </Card.Content>
+              <br />
+              <Card.Content extra>
+                <Icon name="user">0 posts</Icon>
+              </Card.Content>
+            </div>
+          </Card>
+        </div>
+        <div className="userFeed">
+          <Input
+            className="newUserPost"
+            placeholder="What's new with you?"
+            autoFocus
+          />
+          <Feed>
+            <hr />
+            <Feed.Event>
+              <Feed.Label image={octojpg} />
+              <Feed.Content content="You added Elliot Fu to the group Coworkers" />
+              <Feed.Date>4 hours ago</Feed.Date>
+            </Feed.Event>
+          </Feed>
+          <hr />
+        </div>
+      </div>
     );
   }
 }
