12294b12ed71b8fd76c6edaab7e6828c8af6a93b
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-12-14 09:33:21 -0500

    Updates

---
+++ precache-manifest.3ac214f9a425b5bfd89c2f96e409e670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea81571322f78538467ff4e5c7689d30",
+    "url": "/kwitter/static/media/octo1.ea815713.gif"
+  },
+  {
+    "revision": "a9d2acdfec57cab6278b0dd29274d36e",
+    "url": "/kwitter/static/media/octo1.a9d2acdf.png"
+  },
+  {
+    "revision": "1bef2d238c070dadcd30",
+    "url": "/kwitter/static/js/runtime~main.1bef2d23.js"
+  },
+  {
+    "revision": "a75406e0697651ede660",
+    "url": "/kwitter/static/js/main.a75406e0.chunk.js"
+  },
+  {
+    "revision": "19a237301c67b746666e",
+    "url": "/kwitter/static/js/1.19a23730.chunk.js"
+  },
+  {
+    "revision": "a75406e0697651ede660",
+    "url": "/kwitter/static/css/main.fd5f6e30.chunk.css"
+  },
+  {
+    "revision": "a0702968cddc45f969bb81dc2a0d1b32",
+    "url": "/kwitter/index.html"
+  }
+];
