e8b7d28fa1ff85c7a7f99e31ddefc2957efd0fcc
Michael McKenzie <mikemckenzie987@gmail.com>
2018-12-17 11:06:29 -0500

    Updates

---
+++ precache-manifest.989b83f917e00e7ef04a4852530f8f0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea81571322f78538467ff4e5c7689d30",
+    "url": "/kwitter-1/static/media/octo1.ea815713.gif"
+  },
+  {
+    "revision": "a9d2acdfec57cab6278b0dd29274d36e",
+    "url": "/kwitter-1/static/media/octo1.a9d2acdf.png"
+  },
+  {
+    "revision": "786c0cc8b82435b853fe",
+    "url": "/kwitter-1/static/js/runtime~main.786c0cc8.js"
+  },
+  {
+    "revision": "5d15b1939c9cd8d24153",
+    "url": "/kwitter-1/static/js/main.5d15b193.chunk.js"
+  },
+  {
+    "revision": "19a237301c67b746666e",
+    "url": "/kwitter-1/static/js/1.19a23730.chunk.js"
+  },
+  {
+    "revision": "5d15b1939c9cd8d24153",
+    "url": "/kwitter-1/static/css/main.fa69c07e.chunk.css"
+  },
+  {
+    "revision": "2db282eb0976993c2827defe997d303c",
+    "url": "/kwitter-1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kwitter/precache-manifest.860736b3b5a02765f0c682f10101fe04.js"
+  "/kwitter-1/precache-manifest.989b83f917e00e7ef04a4852530f8f0b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kwitter/index.html", {
+workbox.routing.registerNavigationRoute("/kwitter-1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
